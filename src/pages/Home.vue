13d89fd2cc6424f97a1322ec0b0905d2769fe6c9
WangDanmeng <WangDanmeng98@163.com>
2023-02-08 22:29:22 +0800

    布局与适配

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="x">home</div>
+</template>
+
+<script setup>
+import { } from 'vue'
+</script>
+
+<style lang="scss" scoped>
+@import './../shared/helper';
+
+.x {
+  width: rem(367);
+  height: rem(345);
+  border: 1px solid red;
+}
+</style>
