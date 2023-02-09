13e717fb12ea8ecf86a47929ba98254d78b89899
WangDanmeng <WangDanmeng98@163.com>
2023-02-09 13:56:09 +0800

    完成 Chart5

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,7 +10,9 @@
         <Chart3 />
         <Chart4 />
       </section>
-      <section className="bordered section3"></section>
+      <section className="bordered section3">
+        <Chart5 />
+      </section>
       <section className="bordered section4"></section>
       <section className="bordered section5"></section>
     </main>
@@ -24,6 +26,7 @@ import Chart1 from '../components/Chart1.vue'
 import Chart2 from '../components/Chart2.vue'
 import Chart3 from '../components/Chart3.vue'
 import Chart4 from '../components/Chart4.vue'
+import Chart5 from '../components/Chart5.vue'
 import headerBg from '../assets/img/header.png'
 const year = new Date().getFullYear();
 </script>
