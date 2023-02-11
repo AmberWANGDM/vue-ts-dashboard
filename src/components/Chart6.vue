a6ecd000b0fdeb190e67d33a8cb20db13960e31f
WangDanmeng <WangDanmeng98@163.com>
2023-02-12 00:44:26 +0800

    完成地图展示

--- src/components/Chart6.vue
+++ src/components/Chart6.vue
@@ -1,60 +1,66 @@
 <template>
   <div class="bordered 籍贯">
     <h2>全市犯罪人员籍贯分布地</h2>
-    <div class="chart" ref="divRef"></div>
+    <div class="wrapper">
+      <div class="chart" ref="divRef"></div>
+      <div class="legend bordered">
+        <span class="icon" :style="{ background: colors['青海省'] }" />甘岭籍
+        <span class="icon" :style="{ background: colors['浙江省'] }" />边城籍
+        <span class="icon" :style="{ background: colors['四川省'] }" />寒国籍
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import * as echarts from 'echarts';
-import { px } from '../shared/px';
 import { createEchartsOptions } from '../shared/create-echarts-options';
+import china from '../geo/china.json'
+
+const colors = { '青海省': '#BB31F7', '浙江省': '#15B8FD', '四川省': '#06E1EE' };
 const divRef = ref<HTMLDivElement>()
 onMounted(() => {
   if (!divRef.value) return
   var myChart = echarts.init(divRef.value);
+  echarts.registerMap('CN', china) // https://echarts.apache.org/zh/option.html#geo.map
   myChart.setOption(createEchartsOptions({
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
-      splitLine: { show: true, lineStyle: { color: '#073E78' } },
-      axisTick: { show: false },
-      axisLine: { show: false },
-    },
-    yAxis: {
-      type: 'value',
-      splitLine: { lineStyle: { color: '#073E78' } },
-      axisLabel: {
-        formatter(val: number) {
-          return val * 100 + '%';
+    xAxis: { show: false },
+    yAxis: { show: false },
+    geo: {
+      tooltip: {
+        show: true
+      },
+      map: 'CN', // registerMap对应值
+      itemStyle: {
+        areaColor: 'transparent',
+        borderColor: '#2b63b6'
+      },
+      // 高亮颜色
+      emphasis: {
+        itemStyle: {
+          color: '#2b63b6'
         }
-      }
+      },
     },
-    series: [{
-      name: '故意伤人',
-      type: 'line',
-      data: [
-        0.15, 0.13, 0.11,
-        0.13, 0.14, 0.15,
-        0.16, 0.18, 0.21,
-        0.19, 0.17, 0.16,
-        0.15
-      ],
-      symbol: 'circle',
-      symbolSize: px(12),
-      lineStyle: { width: px(2) },
-      areaStyle: {
-        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-          offset: 0,
-          color: '#414a9f'
-        }, {
-          offset: 1,
-          color: '#1b1d52'
-        }]),
-      }
-    }]
+    series: {
+      type: 'effectScatter',
+      coordinateSystem: 'geo',
+      data: [{ name: '浙江', value: [120.153576, 30.287459, 20] },
+      { name: '四川', value: [104.065735, 30.659462, 30] },
+      { name: '甘肃', value: [103.823557, 36.058039, 1] }],
+      symbolSize: function (params: number[]) {
+        return (params[2] / 100) * 15 + 5;
+      },
+      itemStyle: {
+        shadowBlur: 10,
+        shadowColor: '#333'
+      },
+      emphasis: {
+        scale: true
+      },
+      zlevel: 1
+    }
   }));
 })
 </script>
