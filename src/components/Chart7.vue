70ba850e7dee4fa53c8c61312156f09a9f9b243e
WangDanmeng <WangDanmeng98@163.com>
2023-02-18 22:53:08 +0800

    完成 chart7-9

--- src/components/Chart7.vue
+++ src/components/Chart7.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="年龄段-图1">7
-    <div class="chart" ref="divRef"></div>
+  <div class="年龄段-图1">
+    <div class="chart">
+      <div class="main" ref="divRef" />
+      <div class="text">性别</div>
+    </div>
+    <div class="legend">
+      <span class="male" />男
+      <span class="female" />女
+    </div>
   </div>
 </template>
 
@@ -8,14 +15,42 @@
 import * as echarts from 'echarts';
 import { createEchartsOptions } from '../shared/create-echarts-options';
 import { onMounted, ref } from 'vue'
+import { px } from '../shared/px';
 const divRef = ref<HTMLDivElement>()
 onMounted(() => {
-  if (!divRef.value) return
-  var myChart = echarts.init(divRef.value)
-  myChart.setOption(createEchartsOptions({}))
+  var myChart = echarts.init(divRef.value);
+  myChart.setOption(createEchartsOptions({
+    color: ['#8D70F8', '#33A4FA'],
+    xAxis: { show: false },
+    yAxis: { show: false },
+    legend: { show: false },
+    series: [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius: ['75%', '90%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: true, position: 'inside', textStyle: { color: 'white', fontSize: px(20) },
+          formatter(options) {
+            return options.value * 100 + '%';
+          }
+        },
+        labelLine: { show: false },
+        itemStyle: {
+          borderColor: '#0F113A',
+          borderWidth: px(4)
+        },
+        data: [
+          { value: 0.2, name: '女' },
+          { value: 0.8, name: '男' },
+        ]
+      }
+    ]
+  }));
 })
 </script>
 
 <style lang="scss" scoped>
-
+@import '../assets/css/home.scss';
 </style>
--- src/components/Chart9.vue
+++ src/components/Chart9.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="年龄段-图3">
+    <h3>犯罪年龄趋势图</h3>
     <div class="chart" ref="divRef"></div>
   </div>
 </template>
@@ -8,14 +9,54 @@
 import * as echarts from 'echarts';
 import { createEchartsOptions } from '../shared/create-echarts-options';
 import { onMounted, ref } from 'vue'
+import { px } from '../shared/px';
 const divRef = ref<HTMLDivElement>()
 onMounted(() => {
   if (!divRef.value) return
   var myChart = echarts.init(divRef.value)
-  myChart.setOption(createEchartsOptions({}))
+  myChart.setOption(createEchartsOptions({
+    color: '#F7A110',
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: [0, 18, 28, 38, 48, 58, 68, 78],
+      splitLine: { show: true, lineStyle: { color: '#073E78' } },
+      axisTick: { show: false },
+      axisLine: { show: false },
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: { lineStyle: { color: '#073E78' } },
+      axisLabel: {
+        formatter(val) {
+          return val * 100 + '%';
+        }
+      }
+    },
+    series: [{
+      type: 'line',
+      data: [
+        0.19, 0.20, 0.26,
+        0.35, 0.26, 0.20,
+        0.08, 0.06
+      ],
+      symbol: 'circle',
+      symbolSize: px(12),
+      lineStyle: { width: px(2) },
+      areaStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+          offset: 0,
+          color: '#F7A110'
+        }, {
+          offset: 1,
+          color: '#1B1D52'
+        }]),
+      }
+    }]
+  }));
 })
 </script>
 
 <style lang="scss" scoped>
-
+@import '../assets/css/home.scss';
 </style>
