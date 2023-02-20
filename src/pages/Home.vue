733b1b6507359ed26bf929cc314d428eb9cd7457
WangDanmeng <WangDanmeng98@163.com>
2023-02-20 16:38:58 +0800

    完成剩余图表

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,14 +6,14 @@
         <Chart1 />
         <Chart2 />
       </section>
-      <section className="bordered section2">
+      <section class="bordered section2">
         <Chart3 />
         <Chart4 />
       </section>
-      <section className="bordered section3">
+      <section class="bordered section3">
         <Chart5 />
       </section>
-      <section className="bordered section4">
+      <section class="bordered section4">
         <Chart6 />
         <div class="bordered 年龄段">
           <h2>犯罪人员年龄段分布</h2>
@@ -24,11 +24,29 @@
           </div>
         </div>
       </section>
-      <section className="bordered section5"></section>
+      <section class="bordered section5">
+        <div class="bordered row1 案发类型">
+          <h2>案发类型统计</h2>
+          <div class="charts">
+            <Chart10 />
+            <Chart11 />
+          </div>
+        </div>
+        <div class="bordered row2 案发街道">
+          <h2>案发街道统计</h2>
+          <div class="charts">
+            <Chart12 />
+            <Chart13 />
+          </div>
+        </div>
+        <div class="bordered row3 作案手段">
+          <h2>作案手段分析</h2>
+          <Chart14 />
+        </div>
+      </section>
     </main>
     <footer>&copy; danmeng 2023-{{ year }}</footer>
   </div>
-
 </template>
 
 <script setup lang="ts">
@@ -41,6 +59,11 @@ import Chart6 from '../components/Chart6.vue'
 import Chart7 from '../components/Chart7.vue'
 import Chart8 from '../components/Chart8.vue'
 import Chart9 from '../components/Chart9.vue'
+import Chart10 from '../components/Chart10.vue'
+import Chart11 from '../components/Chart11.vue'
+import Chart12 from '../components/Chart12.vue'
+import Chart13 from '../components/Chart13.vue'
+import Chart14 from '../components/Chart14.vue'
 
 import headerBg from '../assets/img/header.png'
 const year = new Date().getFullYear();
